f88b6043e996be749152ed8e2d112d76a2543aa3
Itay Shakury <itay.shakury@aquasec.com>
2019-09-03 18:10:51 +0300

    rearrange kube/docker config

--- windows/wsl-ubuntu/wsl-docker.sh
+++ windows/wsl-ubuntu/wsl-docker.sh
@@ -1,7 +1,3 @@
 #https://nickjanetakis.com/blog/setting-up-docker-for-windows-and-wsl-to-work-flawlessly
-
 sudo mkdir /c
 sudo mount --bind /mnt/c /c
-
-echo -e "\nexport DOCKER_HOST=tcp://localhost:2375" >> ~/.shell_profile
-export KUBECONFIG=$(ls $(wslpath $(cmd.exe /C "echo %USERPROFILE%" | tr -d '\r'))/.kube/config* -1 | tr '\n' ':')
