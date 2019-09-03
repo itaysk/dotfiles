8046467e357035572e49b3630e9e3fdd90b32366
Itay Shakury <itay.shakury@aquasec.com>
2019-09-03 14:25:45 +0300

    fix append to existing file

--- windows/wsl-ubuntu/wsl-docker.sh
+++ windows/wsl-ubuntu/wsl-docker.sh
@@ -3,5 +3,5 @@
 sudo mkdir /c
 sudo mount --bind /mnt/c /c
 
-echo "export DOCKER_HOST=tcp://localhost:2375" >> ~/.shell_profile
+echo -e "\nexport DOCKER_HOST=tcp://localhost:2375" >> ~/.shell_profile
 export KUBECONFIG=$(ls $(wslpath $(cmd.exe /C "echo %USERPROFILE%" | tr -d '\r'))/.kube/config* -1 | tr '\n' ':')
