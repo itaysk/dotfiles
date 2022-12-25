537ae5ae36805af360f01a1c30ba7baf580cc36c
Itay Shakury <itay@itaysk.com>
2022-12-25 14:54:25 +0200

    replace karabiner with macos settings

--- mac-login.sh
+++ mac-login.sh
@@ -1,8 +1,1 @@
 #! /bin/bash
-
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Scroll Reverser", path:"/Applications/Scroll Reverser.app", hidden:true}'
-#osascript -e 'tell application "System Events" to make login item at end with properties {name: "OneDrive", path:"/Applications/OneDrive.app", hidden:true}'
-#osascript -e 'tell application "System Events" to make login item at end with properties {name: "Spectacle", path:"/Applications/Spectacle.app", hidden:false}'
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Karabiner-Elements", path:"/Applications/Karabiner-Elements.app", hidden:true}'
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Alfred 4", path:"/Applications/Alfred 4.app", hidden:false}'
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "Amethyst", path:"/Applications/Amethyst.app", hidden:false}'
